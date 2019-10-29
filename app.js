9d2190c2f706c6d54fafc69e7fbdcde763070bf3
dyaksa <diasnour0395@gmail.com>
2019-10-29 19:44:16 +0700

    tambahkan style dan fungsi hapus

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const moment = require("moment");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const _ = require("lodash");
+
 
 let app = express();
 app.set("view engine", "ejs");
@@ -14,60 +17,115 @@ moment.locale("id");
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }).then(() => {
     console.log("database dihubungkan");
 }).catch((err) => {
     console.log(err);
 });
 
-const itemsSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
+const itemSchema = Schema({
+    name: String
 });
 
-const List = mongoose.model("Lists", itemsSchema);
 
-const workLists = [];
+const listSchema = Schema({
+    name: String,
+    items: [itemSchema]
+})
+
+const Items = mongoose.model("Item", itemSchema);
+const Lists = mongoose.model("List", listSchema);
+
+const item1 = new Items({
+    name: "Welcome to your todolist"
+});
+
+const item2 = new Items({
+    name: "Hit the + button add new item"
+});
+
+const item3 = new Items({
+    name: "<-- Hit this to delete item"
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req, res) {
     let day = moment().format("dddd, DD MMMM");
-    let lists = List.find({}, function (err, results) {
+    Items.find({}, function (err, items) {
+        if (err) throw err;
         res.render("list", {
             listTitle: day,
-            lists: results
+            items: items
         });
     });
-
 });
 
 app.post("/", function (req, res) {
-    const itemName = req.body.list;
-    const itemNew = new List({
-        name: itemName
+    const submit = req.body.submit;
+    const inputItem = req.body.item;
+    const today = moment().format("dddd, DD MMMM");
+
+    const item = new Items({
+        name: inputItem
     });
-    itemNew.save({
-        validateBeforeSave: true
-    }, function (err) {
-        if (!err) {
+
+    if (submit === today) {
+        item.save();
+        res.redirect("/");
+    } else {
+        Lists.findOne({
+            name: submit
+        }, function (err, list) {
+            if (err) throw err;
+            list.items.push(item);
+            list.save(function (err) {
+                if (err) throw err;
+                res.redirect("/" + submit);
+            });
+        });
+    }
+});
+
+app.post("/delete", function (req, res) {
+    const id = req.body.checkbox;
+    const day = moment().format("dddd, DD MMMM");
+    const today = req.body.titleName;
+    if (day === today) {
+        Items.findByIdAndRemove(id, function (err) {
+            if (err) throw err;
+            console.log("success delete");
             res.redirect("/");
-            console.log("data tersimpan");
+        });
+    }
+
+});
+
+app.get("/:customList", function (req, res) {
+    const customList = _.toLower(req.params.customList);
+    Lists.findOne({
+        name: customList
+    }, function (err, list) {
+        if (!err) {
+            if (!list) {
+                const list = new Lists({
+                    name: customList,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customList);
+            } else {
+                res.render("list", {
+                    listTitle: list.name,
+                    items: list.items
+                });
+            }
         }
     });
-});
 
-app.delete("/delete", function (req, res) {
-    console.log("Hello World");
-});
 
-app.get("/work", function (res, res) {
-    let title = "Work";
-    res.render("list", {
-        listTitle: title,
-        lists: workLists
-    });
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("port 3000"));
